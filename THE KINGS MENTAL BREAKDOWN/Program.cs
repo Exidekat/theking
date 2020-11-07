e3768150411c112a3c58060c7d7e0f56e59c620d
dorsamousavi <dorsa.m1356@gmail.com>
2020-11-07 12:01:06 -0800

    remove flag

--- THE KINGS MENTAL BREAKDOWN/Program.cs
+++ THE KINGS MENTAL BREAKDOWN/Program.cs
@@ -9,11 +9,6 @@ using CTRE.Phoenix;
 
 namespace THE_KINGS_MENTAL_BREAKDOWN
 {
-    public enum FlagState
-    {
-        TURNING,
-        IDLE
-    }
     public enum DriveState
     {
         PRECISE, //slower, lower gain
@@ -29,11 +24,20 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
 
     public class Program
     {
-        static Stopwatch stopwatch = new Stopwatch();
+        private static Stopwatch stopwatch = new Stopwatch();
 
-        private static ElevatorState elevatorState = ElevatorState.ERROR;
-        private static DriveState driveState = DriveState.PRECISE;  //creates a variable with a Drivestate type, makes the starting value PRECISE
+        private static ElevatorState elevatorState = ElevatorState.ERROR; //creates a variable with a ElevatorState type, makes the starting value ERROR 
+        private static DriveState driveState = DriveState.PRECISE;  //creates a variable with a DriveState type, makes the starting value PRECISE
         private static FlagState flagState = FlagState.IDLE; //creates a variable with a FlagState type, makes the starting value IDLE
+
+        //creating a GameController object
+        private static GameController gamepad1 = new GameController(new CTRE.Phoenix.UsbHostDevice(0));
+
+        //creating TalonSRX objects(drivetrain(right and left), climber or elevator, and flag raise)
+        private static TalonSRX leftDriveTalon = new TalonSRX(0);
+        private static TalonSRX rightDriveTalon = new TalonSRX(1);
+        private static TalonSRX climbTalon = new TalonSRX(2);
+        private static TalonSRX flagTalon = new TalonSRX(3);
 
         private static float turnAxis; //x-axis of stick 
         private static float forwardAxis; // y-axis of stick 
@@ -45,6 +49,7 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
 
         public static void Main()
         {
+            //runs both PrintTime and Everything
             stopwatch.Start();
             var t1 = new Thread(Everything);
             t1.Start();
@@ -54,113 +59,86 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
 
         static void Everything()
         {
+            // creates a variable for the time(seconds)
             float time = stopwatch.Duration; 
 
-            /* Creating our gamepad */
-            GameController gamepad1 = new GameController(new CTRE.Phoenix.UsbHostDevice(0));
+            // prevents unexpected behavior 
+            rightDriveTalon.ConfigFactoryDefault();
+            leftDriveTalon.ConfigFactoryDefault();
+            climbTalon.ConfigFactoryDefault();
+            flagTalon.ConfigFactoryDefault();
 
-            //creating TalonSRX objects(dirvetrain, climber or elevator, and flag raise)
-            TalonSRX leftDriveTalon = new TalonSRX(0);
-            TalonSRX rightDriveTalon = new TalonSRX(1);
-            TalonSRX climbTalon = new TalonSRX(2);
-            TalonSRX flagTalon = new TalonSRX(3);
-
-            //loops forever 
-            while (true)
+            // not finished autonomous period 
+            if (time < 30)
             {
-                rightDriveTalon.ConfigFactoryDefault();
-                leftDriveTalon.ConfigFactoryDefault();
-                climbTalon.ConfigFactoryDefault();
-                flagTalon.ConfigFactoryDefault();
+                Debug.Print("Auton is attempting to E");
 
-                /* this is checked periodically*/
-                if (gamepad1.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.Connected)
+                leftDriveTalon.Set(ControlMode.PercentOutput, 0.5); //sets left drive talon to 50%
+                rightDriveTalon.Set(ControlMode.PercentOutput, 0.5); //sets right drive talon to 50%
+
+                Thread.Sleep(10);
+
+                Debug.Print("Auton ending, E complete.");
+                leftDriveTalon.Set(ControlMode.PercentOutput, 0.0); //sets left drive talon to 0%
+                rightDriveTalon.Set(ControlMode.PercentOutput, 0.0); //sets right drive talon to 0%
+            }
+
+            if (gamepad1.GetConnectionStatus() == CTRE.Phoenix.UsbDeviceConnection.)
+            { 
+                // print axis value ???
+                Debug.Print("axis:" + gamepad1.GetAxis(1));
+
+                // allows motor control 
+                CTRE.Phoenix.Watchdog.Feed();
+
+                // Teleoperated 
+                // determine inputs
+                forwardAxis = gamepad1.GetAxis(1);
+                turnAxis = gamepad1.GetAxis(2);
+
+                // accounting for forward and turn b4 Eing into the talons
+                finalLeftTalonValue = (forwardAxis * forwardGain) + (turnAxis * turnGain);
+                finalRightTalonValue = (forwardAxis * forwardGain) - (turnAxis * turnGain);
+
+                // pass motor value to Talons 
+                leftDriveTalon.Set(ControlMode.PercentOutput, finalLeftTalonValue);
+                rightDriveTalon.Set(ControlMode.PercentOutput, finalRightTalonValue);
+
+                 // Elevator controls 
+                if (gamepad1.GetButton(6808099))
                 {
-                    /* print axis value */
-                    Debug.Print("axis:" + gamepad1.GetAxis(1));
-
-                    /* allow motor control */
-                    CTRE.Phoenix.Watchdog.Feed();
-
+                    elevatorState = ElevatorState.RISE;
+                }
+                else if (gamepad1.GetButton(9897))
+                {
+                    elevatorState = ElevatorState.LOWER;
+                }
+                else
+                {
+                    elevatorState = ElevatorState.IDLE;
                 }
 
-                // not finished autonomous period 
-                if (time < 30)//time < 30
+                // if the button is pressed the drivestate will switch 
+                if (gamepad1.GetButton(1423) && driveState == DriveState.PRECISE)// stick button
                 {
-                    Debug.Print("Auton is attempting to E");
-
-                     leftDriveTalon.Set(ControlMode.PercentOutput, 0.5); //sets left drive talon to 50%
-                     rightDriveTalon.Set(ControlMode.PercentOutput, 0.5); //sets right drive talon to 50%
-
-
-                     /*  Amount of time to wait before repeating the loop */
-                     Thread.Sleep(10);
-
-                    Debug.Print("Auton ending, E complete.");
-                    leftDriveTalon.Set(ControlMode.PercentOutput, 0.0); //sets left drive talon to 0%
-                    rightDriveTalon.Set(ControlMode.PercentOutput, 0.0); //sets right drive talon to 0%
+                    driveState = DriveState.STRONGER;
+                }
+                else if (gamepad1.GetButton(1423) && driveState == DriveState.STRONGER)
+                {
+                    driveState = DriveState.PRECISE;
                 }
 
-
-                else // teleop
+                // If the button is pressed then the flagState with switch 
+                if (gamepad1.GetButton(1232) && flagState == FlagState.IDLE)
                 {
-                    /* NOTE TO HAYDON: ADD THE ACTUAL CONTROLLER AXISES IDS
-                    stick 1 x axis: id 2?
-                    stick 1 y axis: id 1?
-                    stick 2 x axis: id
-                    stick 2 y axis: id
-                    */
-
-
-                    //determine inputs
-                    forwardAxis = gamepad1.GetAxis(1);
-                    turnAxis = gamepad1.GetAxis(2);
-
-                    //accounting for forward and turn b4 Eing into the talons
-                    finalLeftTalonValue = (forwardAxis * forwardGain) + (turnAxis * turnGain);
-                    finalRightTalonValue = (forwardAxis * forwardGain) - (turnAxis * turnGain);
-
-                    /* pass motor value to talons */
-                    leftDriveTalon.Set(ControlMode.PercentOutput, finalLeftTalonValue);
-                    rightDriveTalon.Set(ControlMode.PercentOutput, finalRightTalonValue);
-
-                    /* the climby controls NOTE TO HAYDON: ADD THE ACTUAL CONTROLLER BUTTON IDS*/
-                    if (gamepad1.GetButton(6808099))
-                    {
-                        elevatorState = ElevatorState.RISE;
-                    }
-                    else if (gamepad1.GetButton(9897))
-                    {
-                        elevatorState = ElevatorState.LOWER;
-                    }
-                    else
-                    {
-                        elevatorState = ElevatorState.IDLE;
-                    }
-
-                    //if the button is pressed the drivestate will switch 
-                    if (gamepad1.GetButton(1423) && driveState == DriveState.PRECISE)//stick button
-                    {
-                        driveState = DriveState.STRONGER;
-                    }
-                    else if (gamepad1.GetButton(1423) && driveState == DriveState.STRONGER)
-                    {
-                        driveState = DriveState.PRECISE;
-                    }
-
-
-                    if (gamepad1.GetButton(1232) && flagState == FlagState.IDLE)
-                    {
-                        flagState = FlagState.TURNING;
-                    }
-                    else if (gamepad1.GetButton(1232) && flagState == FlagState.TURNING)
-                    {
-                        flagState = FlagState.IDLE;
-                    }
-
+                    flagState = FlagState.TURNING;
+                }
+                else if (gamepad1.GetButton(1232) && flagState == FlagState.TURNING)
+                {
+                    flagState = FlagState.IDLE;
                 }
 
-                /* elevator state machine garbo */
+                // elevator state machine
                 switch (elevatorState)
                 {
                     case ElevatorState.IDLE:
@@ -183,7 +161,7 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
                         break;
                 }
 
-                //drive gain state machine 
+                // drive gain state machine 
                 switch (driveState)
                 {
                     case DriveState.PRECISE:
@@ -201,7 +179,7 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
                         break;
                 }
 
-                //flag state machine 
+                // flag state machine 
                 switch (flagState)
                 {
                     case FlagState.TURNING:
@@ -220,6 +198,7 @@ namespace THE_KINGS_MENTAL_BREAKDOWN
             }
         }
 
+        //prints the time in seconds 
         static void PrintTime()
         {
             bool timer = true;
